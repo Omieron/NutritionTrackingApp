74d758fabaa81b9690ce69108bc76f6ed721815e
sarazebardast <sara.zebardast03@gmail.com>
2024-12-07 20:52:44 +0300

    added activities

---
+++ app/src/main/java/com/group20/nutritiontrackingapp/PersonActivity.kt
@@ -0,0 +1,13 @@
+package com.group20.nutritiontrackingapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PersonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
