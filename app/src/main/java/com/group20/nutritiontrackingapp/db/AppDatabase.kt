12aefee494cb900740b6bb4d4c457eef84f99013
sarazebardast <sara.zebardast03@gmail.com>
2024-12-10 23:32:44 +0300

    added some strings and minor fix, added Person class to the database

--- app/src/main/java/com/group20/nutritiontrackingapp/db/AppDatabase.kt
+++ app/src/main/java/com/group20/nutritiontrackingapp/db/AppDatabase.kt
@@ -3,9 +3,10 @@ package com.group20.nutritiontrackingapp.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Recipe::class, Exercise::class], version = 2)
+@Database(entities = [Recipe::class, Exercise::class, Person::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun recipeDao(): RecipeDAO
     abstract fun exerciseDao(): ExerciseDAO
+    abstract fun personDao(): PersonDAO
 }
