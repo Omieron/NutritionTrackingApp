a9b9f3be5ac0ffb2951c873494058c648d08dd5a
Ömer Faruk Asil <asilo02@hotmail.com>
2024-12-25 15:49:16 +0300

    donutChart is working

--- app/src/main/java/com/group20/nutritiontrackingapp/ReportActivity.kt
+++ app/src/main/java/com/group20/nutritiontrackingapp/ReportActivity.kt
@@ -1,24 +1,75 @@
 package com.group20.nutritiontrackingapp
 
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.room.Room
+import com.group20.nutritiontrackingapp.chart.DonutChart
 import com.group20.nutritiontrackingapp.databinding.ActivityReportBinding
+import com.group20.nutritiontrackingapp.db.AppDatabase
+import com.group20.nutritiontrackingapp.db.Meal
+import com.group20.nutritiontrackingapp.util.Constants
 
 class ReportActivity : AppCompatActivity() {
-
     // Variables
-    lateinit var binding: ActivityReportBinding
-
+    private lateinit var binding: ActivityReportBinding
+    private lateinit var donutChart: DonutChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Binding section
         binding = ActivityReportBinding.inflate(layoutInflater)
+        //donutChart = findViewById(R.id.donutChart)
         setContentView(binding.root)
+        enableEdgeToEdge()
+
+        val db: AppDatabase by lazy {
+            Room.databaseBuilder(
+                this,
+                AppDatabase::class.java,
+                Constants.DATABASE_NAME
+            )
+                .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+
+        val meals = db.mealDao().getAllMeals()
+
+        if(meals.isNotEmpty())
+            getMealsGiveMacros(meals)
 
         //Listeners
         binding.reportBackBtn.setOnClickListener {
             finish()
         }
+    }
+
+    private fun getMealsGiveMacros(meals : MutableList<Meal>) {
+        var totalProtein = 0.0
+        var totalCarbs = 0.0
+        var totalFat = 0.0
+
+        val carbText : TextView = findViewById(R.id.carbRate)
+        val proText : TextView = findViewById(R.id.proRate)
+        val fatText : TextView = findViewById(R.id.fatRate)
+
+        for (meal in meals) {
+            totalProtein += meal.protein
+            totalCarbs += meal.carbs
+            totalFat += meal.fat
+        }
+
+        var totalMacros = totalProtein + totalCarbs + totalFat
+
+        val proteinPercentage = (totalProtein / totalMacros * 100).toFloat()
+        val carbsPercentage = (totalCarbs / totalMacros * 100).toFloat()
+        val fatPercentage = (totalFat / totalMacros * 100).toFloat()
+
+        carbText.text = String.format("%.2f", totalCarbs) + "g"
+        proText.text = String.format("%.2f", totalProtein) + "g"
+        fatText.text = String.format("%.2f", totalFat) + "g"
+        DonutChart.updateData(carbsPercentage,proteinPercentage,fatPercentage)
     }
 }
--- app/src/main/java/com/group20/nutritiontrackingapp/chart/DonutChart.kt
+++ app/src/main/java/com/group20/nutritiontrackingapp/chart/DonutChart.kt
@@ -10,32 +10,44 @@ import android.util.AttributeSet
 import androidx.core.content.ContextCompat
 import com.group20.nutritiontrackingapp.R
 
-class DonutChart (context: Context, attrs: AttributeSet?) : View(context, attrs) {
+class DonutChart(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+
     private val paint = Paint().apply {
         isAntiAlias = true
         style = Paint.Style.STROKE
     }
 
+    private var colors = listOf(
+        ContextCompat.getColor(context, R.color.Pastel_Purple),
+        ContextCompat.getColor(context, R.color.Pastel_Red),
+        ContextCompat.getColor(context, R.color.Pastel_Blue)
+    )
+
+    companion object {
+
+        private var data = listOf(33f, 33f, 33f)
+
+        fun updateData(carb: Float, pro: Float, fat: Float) {
+            data = listOf(carb, pro, fat)
+
+        }
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
-        val data = listOf(40f, 30f, 30f) // Veri yüzdeleri
-        val colors = listOf(ContextCompat.getColor(context, R.color.Pastel_Cream), ContextCompat.getColor(context, R.color.Pastel_Green), ContextCompat.getColor(context, R.color.Pastel_Blue)) // Her dilim için renkler
 
         val total = data.sum()
         var startAngle = 0f
 
-        // Grafik çizim alanını tanımla
         val rect = RectF(150f, 150f, width - 150f, height - 150f)
 
-        // Stroke genişliğini artırarak kalınlığı değiştir
-        val strokeWidth = 170f // Donut kalınlığı
+        val strokeWidth = 170f
         paint.strokeWidth = strokeWidth
 
         for (i in data.indices) {
-            paint.color = colors[i] // Renk seçimi
-            val sweepAngle = (data[i] / total) * 360 // Veri yüzdesine göre açı hesaplama
-            canvas.drawArc(rect, startAngle, sweepAngle, false, paint) // Yay çizimi
+            paint.color = colors[i]
+            val sweepAngle = (data[i] / total) * 360
+            canvas.drawArc(rect, startAngle, sweepAngle, false, paint)
             startAngle += sweepAngle
         }
     }
